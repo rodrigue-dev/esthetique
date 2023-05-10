6975db7d7ac2c770adc5668375bd7c34dff62cb9
rodriguembah13 <rodriguembah13@gmail.com>
2023-05-10 12:09:33 +0100

    init

---
+++ database/migrations/2023_05_05_075701_create_facture.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factures', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('numero');
+            $table->date('date_facture');
+            $table->double('montantht');
+            $table->double('tva');
+            $table->double('montantttc');
+            $table->double('reduction');
+            $table->double('adresse');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('facture');
+    }
+};
---
+++ resources/views/back/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('back.base')
+
+@section('content')
+    <div class="content-page">
+        <div class="content">
+        @include("back._partials.errors-and-messages")
+        <!-- Start Content-->
+            <div class="container-fluid">
+                <div class="row mt-3">
+
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <div class="card">
+
+                            <div class=" table-responsive">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
